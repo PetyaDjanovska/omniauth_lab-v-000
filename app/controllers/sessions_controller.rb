9f3faa0d0294d77f35933f4eb4c7968de13e8822
Petya Djanovska <petya.djanovska@gmail.com>
2018-07-27 16:17:47 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_or_create_by(uid: auth['uid'])
-      @user.name = auth['info']['name']
-      # @user.email = auth['info']['email']
+    @user = User.find_or_create_by(uid: auth['uid']) do |u|
+      u.name = auth['info']['name']
+      u.email = auth['info']['email']
+      u.image = auth['info']['image']
+    end
 
-      session[:user_id] = @user.id
+    session[:user_id] = @user.id
   end
 
 
